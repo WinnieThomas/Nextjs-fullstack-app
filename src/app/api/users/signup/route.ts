a9b5d95afa8a4ea3cb0783530119a5694b6346de
WinnieThomas <winnieantony1988@gmail.com>
2023-09-16 11:43:18 +0100

    added login

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,12 +1,13 @@
 import {connect} from '@/dbConfig/dbConfig';
 import User from '@/models/userModel';
 import { NextRequest,NextResponse } from 'next/server';
+import bcryptjs from 'bcryptjs';
 
 
 connect();
 
 export async function POST(request:NextRequest){
-  const bcryptjs = require("bcryptjs");
+  
   try{
       const reqBody = await request.json();
       const{username,email,password} = reqBody;
